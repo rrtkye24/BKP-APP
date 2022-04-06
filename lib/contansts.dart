390a74f004eda4f6c402f693dba1983e57111882
rrtkye24 <retha.teja2403@gmail.com>
2022-04-06 14:24:18 +0700

    Splash screen

---
+++ lib/contansts.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kBackgroudColor = Color(0xFFEEC286);
+const kBackgroudColor1 = Color(0xFF8E590A);
+const kBackgroudColor2 = Color(0xFFA89682);
+const kBackgroudColor3 = Color(0xFF5A483E);
+const kBackgroudColor4 = Color(0xFF926A39);
+const kBackgroudColor5 = Color(0xFFEFE4D2);
---
+++ lib/ui/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(height: size.height),
+        ],
+      ),
+    );
+  }
+}
